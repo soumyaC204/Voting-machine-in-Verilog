06d753445cbd8378cdef67f6fc9f6f160dc6cc36
soumyaC204 <151494995+soumyaC204@users.noreply.github.com>
2024-09-16 22:37:03 +0530

    Create votelogger.v

---
+++ votelogger.v
@@ -0,0 +1,19 @@
+module votelogger(input clk, input reset, input mode, input cand1_vote_valid, input cand2_vote_valid, input cand3_vote_valid, input cand4_vote_valid, output reg [7:0] cand1_vote_recvd, output reg [7:0] cand2_vote_recvd, output reg [7:0] cand3_vote_recvd, output reg [7:0] cand4_vote_recvd);
+  always @(posedge clk) begin
+    if (reset) begin
+      cand1_vote_recvd <= 0;
+      cand2_vote_recvd <= 0;
+      cand3_vote_recvd <= 0;
+      cand4_vote_recvd <= 0;
+    end else begin
+      if (cand1_vote_valid & mode == 0)
+        cand1_vote_recvd <= cand1_vote_recvd + 1;  
+      if (cand2_vote_valid & mode == 0)
+        cand2_vote_recvd <= cand2_vote_recvd + 1;  
+      if (cand3_vote_valid & mode == 0)
+        cand3_vote_recvd <= cand3_vote_recvd + 1;  
+      if (cand4_vote_valid & mode == 0)
+        cand4_vote_recvd <= cand4_vote_recvd + 1;  
+    end
+  end
+endmodule
